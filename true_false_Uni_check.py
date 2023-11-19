52e8d64ae4910184e6ab82572db456dcaf8cf19d
YousefMoussa <129340216+YousefMoussa@users.noreply.github.com>
2023-11-19 01:36:07 -0700

    S

--- true_false_Uni_check.py
+++ true_false_Uni_check.py
@@ -1,4 +1,4 @@
-self.major_check{'Administration Studies': ,
+major_check = {'Administration Studies': ,
     'Art Studies': ,
     'Aviation': ,
     'Business Studies': ,
@@ -29,3 +29,19 @@ self.major_check{'Administration Studies': ,
     'Tourism and Hospitality': ,
     'Transportation and Logistics': 
 }
+
+
+"Business, Mgt and Public Administration" --> 'Business Studies','Economic Studies','Management Studies','Marketing Studies','Administration Studies','Tourism and Hospitality'
+"Visual & performing Arts, and Comm. Technologies" --> 'Art Studies','Design Studies','Fashion','Journalism and Mass Communication','Languages','Performing Arts'
+"Physical and Life Sciences and Technologies" --> 'Aviation','Energy Studies','Food and Beverage Studies','General Studies','Life Sciences','Natural Sciences'
+"Architecture and Related Technologies" --> 'Construction'
+"Engineering" --> 'Engineering Studies'
+"Agriculture, Natural Resources and Conservation" --> 'Environmental Studies','Sustainability Studies'
+"Nursing" --> 'Health Care'
+'Education' --> 'Education'
+"Humanities" --> 'Humanities Studies','Social Sciences'
+"Law" --> 'Law Studies'
+"Other Health, Parks, Recreation and Fitness" --> 'Sport'
+"Math, Computer and Information Sciences" --> 'Technology Studies'
+"Personal, Protective and Transportation services" --> 'Transportation and Logistics'
+
