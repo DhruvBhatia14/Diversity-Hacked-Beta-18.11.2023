9bb72f95960cbce00e9a6264f2adfb45a5a96b91
YousefMoussa <129340216+YousefMoussa@users.noreply.github.com>
2023-11-18 17:30:11 -0700

    Yousef and Dhruv part without gpt

--- Yousefpart.py
+++ Yousefpart.py
@@ -1,7 +1,24 @@
 class Location:
     def __init__(self, majors_list):
         self.majors = majors_list 
-        self.locpref = ["Edmonton", "Red Deer", "Calgary", "Alberta", "Canada"]
+        self.locpref = []
+        self.province = ["Alberta","British Columbia","Manitoba","New Brunswick","Newfoundland and Labrador","Northwest Territories","Nova Scotia",
+        "Nunavut","Ontario","Prince Edward Island","Quebec","Saskatchewan","Yukon"]
+        self.provcitiesdic = {
+        "Alberta": ["Calgary", "Edmonton", "Red Deer","Lethbridge"],
+        "British Columbia": ["Vancouver", "Victoria", "Surrey"],
+        "Manitoba": ["Winnipeg", "Brandon", "Thompson"],
+        "New Brunswick": ["Fredericton", "Saint John", "Moncton"],
+        "Newfoundland and Labrador": ["St. John's", "Mount Pearl", "Corner Brook"],
+        "Northwest Territories": ["Yellowknife", "Inuvik", "Hay River"],
+        "Nova Scotia": ["Halifax", "Dartmouth", "Sydney"],
+        "Nunavut": ["Iqaluit", "Rankin Inlet", "Arviat"],
+        "Ontario": ["Toronto", "Ottawa", "Mississauga", "Hamilton", "Waterloo"],
+        "Prince Edward Island": ["Charlottetown", "Summerside", "Stratford"],
+        "Quebec": ["Montreal", "Quebec City", "Laval"],
+        "Saskatchewan": ["Saskatoon", "Regina", "Prince Albert"],
+        "Yukon": ["Whitehorse", "Dawson City", "Watson Lake"]
+        }
         self.universities = [
         # Alberta
         "University of Alberta", "University of Calgary", "University of Lethbridge",
@@ -52,34 +69,80 @@ class Location:
         "Yukon College"
         ]
         print(len(self.universities))
-    def extract_distances(self):
-        self.distance_dict = {}
-        for c in range(len(self.locpref)-2):
-            city = self.locpref[c]
-            distance_allowed = input("What distance range in km do you want to allow arround %s? " %city)
-            self.distance_dict[city] = distance_allowed
-        self.distance_dict[self.locpref[-2]] = True
-        self.distance_dict[self.locpref[-1]] = True
-        return self.distance_dict
     
+    def userloc(self):
+        self.loc = str(input("Where do you currently stay? "))
+        return self.loc
+    
+    def locquestions(self):
+        self.provlocs = "The provinces you can choose from are:\n"
+        for i in range(len(self.province)-1):
+            self.provlocs += self.province[i]+", "
+        self.provlocs = self.provlocs + self.province[len(self.province)-1]+"."
+        print(self.provlocs)
+        tag = True
+        while tag:
+            try:
+                x=(input("Which province would you prefer for University? ")).title()
+                assert (x in self.province)
+                tag=False
+            except AssertionError:
+                print("We do not have that in our database right now, please consider a new province.")
+        self.locpref.append(x)
+        for items in self.provcitiesdic:
+            if x == items:
+                y = self.provcitiesdic[items]
+                break
+        q = "The cities you can choose from are:\n"
+        for j in range(len(y)-1):
+            q += y[j] + ", "
+        q = q + y[len(y)-1] + "."
+        print(q)
+        s = "Which city would you prefer in "+items+"? "
+        flag = True
+        while flag:
+            try:
+                t = (input(s)).title()
+                assert (t in y)
+                flag = False
+            except AssertionError:
+                    print("We do not have that in our database right now, please consider a new city.")
+        self.locpref.insert(0,t)
+        flag = True
+        while flag:
+            s = "Would you consider any other cities in this province other than "+t+"? (Yes/No) "
+            z = (input(s)).title()
+            if z == "Yes":
+                tag = True
+                while tag:
+                    try:
+                        w = (input("Which other city in this province? ")).title()
+                        assert (w in y)
+                        self.locpref.insert(1,w)
+                        tag = False
+                    except AssertionError:
+                        print("We do not have that in our database right now, please consider a new city.")
+            if z == "No":
+                flag = False
+        self.locpref.append("Canada")
+        print(self.locpref)    
 
     def University_check(self):
-        self.distnace_cleared_university = []
+        self.distance_cleared_university = []
         curr_major = self.majors[0]
         current_locpref = self.locpref[0]
-        dist_allow = self.distance_dict[current_locpref]
         for university in self.universities:
-            #Ask chatgpt if the university is within distance and it has the major and return True or false
+            #Ask chatgpt if the university is in the city it has the major and return True or false
             within_distnace = True
             if within_distnace:
-                self.distnace_cleared_university.append(university)
+                self.distance_cleared_university.append(university)
             else:
                 continue
         
-        return self.distnace_cleared_university
+        return self.distance_cleared_university
 
 x = Location(["eng", "ed", "nurs"])
-x.extract_distances()
+x.locquestions()
 y = x.University_check()
 print(len(y))
 
